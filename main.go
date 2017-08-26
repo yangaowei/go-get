5d1bc68903897c9973ce0305c874f1fbe56320e0
yangaowei <gaoweiyan@sohu-inc.com>
2017-08-26 20:52:58 +0800

    nothing

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 //
 import (
-	"./logs"
+	//"./logs"
 	"./web"
+	"flag"
 )
 
+var (
+	port string
+)
+
+func initFlag() {
+	flag.StringVar(&port, "port", "8002", "server port")
+	flag.Parse()
+}
+
 func main() {
-	logs.Log.Informational("statrt server")
-	web.Run()
+	initFlag()
+	web.Run(port)
 }
