e8e0cfc0ea96fe8378d10fa5f2e584b7af8051f4
yangaowei <gaoweiyan@sohu-inc.com>
2018-01-08 21:40:23 +0800

    添加运行参数

--- main.go
+++ main.go
@@ -2,21 +2,27 @@ package main
 
 //
 import (
-	//"./logs"
+	"./logs"
 	"./web"
 	"flag"
 )
 
 var (
-	port string
+	port    string
+	pattern string //api  cmd
 )
 
 func initFlag() {
 	flag.StringVar(&port, "port", "8002", "server port")
+	flag.StringVar(&pattern, "p", "api", "runing pattern")
 	flag.Parse()
 }
 
 func main() {
 	initFlag()
-	web.Run(port)
+	//
+	logs.Log.Informational("pattern: %s", pattern)
+	if pattern == "api" {
+		web.Run(port)
+	}
 }
